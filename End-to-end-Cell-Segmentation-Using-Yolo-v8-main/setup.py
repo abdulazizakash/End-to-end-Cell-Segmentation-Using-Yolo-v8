cef19b9a780a78022529024d40db454f875b36be
Abdul Aziz Akash <76031119+abdulazizakash@users.noreply.github.com>
2024-11-12 23:44:23 +0600

    Add files via upload

---
+++ End-to-end-Cell-Segmentation-Using-Yolo-v8-main/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Abdul Aziz Akash',
+    author_email= 'abdulaziz.akash5090@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
